e8a541a4749554a5eee2f3dbc66303885c0c4fa7
Phat Vuong <vvp301097@gmail.com>
2024-10-07 22:53:16 +0700

    Initial Commit

---
+++ Custom Camera/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Custom Camera
+//
+//  Created by Phat Vuong Vinh on 7/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Custom Camera/Custom_CameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_CameraApp.swift
+//  Custom Camera
+//
+//  Created by Phat Vuong Vinh on 7/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Custom_CameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
